5b53afcc39019048688cfc4bd65aa063d1f68c3a
Divedevdone <lelloleo0001@gmail.com>
2025-04-27 10:12:46 -0300

    voltei

--- DockerFile/src/main.py
+++ DockerFile/src/main.py
@@ -12,19 +12,3 @@ class Estudante(BaseModel):
 @app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/funcaoteste")
-async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 57000)}
-
-@app.post("/estudantes/cadastro")
-async def create_estudante(estudante: Estudante):
-    return estudante
-
-@app.put("/estudantes/update/{id_estudante}")
-async def update_item(id_estudante: int):
-    return id_estudante > 0
-
-@app.delete("/estudantes/delete/{id_estudante}")
-async def delete_estudante(id_estudante: int):
-    return id_estudante > 0
