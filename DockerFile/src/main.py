9d23d20f1ae348f255f31006245778fff16fe042
Divedevdone <lelloleo0001@gmail.com>
2025-04-27 10:26:15 -0300

    voltei

--- DockerFile/src/main.py
+++ DockerFile/src/main.py
@@ -1,30 +1,10 @@
-import random
-from fastapi import FastAPI
-from pydantic import BaseModel
+FROM python:3
 
-app = FastAPI()
+WORKDIR /usr/src/app
 
-class Estudante(BaseModel):
-    name: str
-    curso: str
-    ativo: bool
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
 
-@app.get("/helloworld")
-async def root():
-    return {"message": "Hello World"}
+COPY . .
 
-@app.get("/funcaoteste")
-async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 57000)}
-
-@app.post("/estudantes/cadastro")
-async def create_estudante(estudante: Estudante):
-    return estudante
-
-@app.put("/estudantes/update/{id_estudante}")
-async def update_estudante(id_estudante: int):
-    return id_estudante > 0
-
-@app.delete("/estudantes/delete/{id_estudante}")
-async def delete_estudante(id_estudante: int):
-    return id_estudante > 0
+CMD [ "python", "./main.py" ]
