28b0e90fb29ca797d7923f7e1e7a9df1cb14e7cf
Divedevdone <125664463+Divedevdone@users.noreply.github.com>
2025-10-07 17:36:09 -0300

    Delete observacao.sql

--- observacao.sql
+++
@@ -1,6 +0,0 @@
--- Criação da Tabela 'observacao'
-CREATE TABLE observacao (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    observacao TEXT NOT NULL,
-    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
-);
