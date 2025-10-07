ae0c0d3eb146b45cc20190891edbe661eea24159
Divedevdone <125664463+Divedevdone@users.noreply.github.com>
2025-10-07 17:23:57 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import random
-
-app = FastAPI()
-
-
-@app.get("/funcaoteste")
-async def funcaoteste():
-    return{"teste": True, "num_aleatorio": random.randint(0,1000)}
